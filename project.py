a7436cebb17d9c1d6598700553c44e2470cc7f12
БакгофТаисия <bakgoft@gmail.com>
2024-03-06 14:00:36 +0500

    dictionary update

--- project.py
+++ project.py
@@ -41,18 +41,18 @@ def get_characters(sentences):
 				else:
 					characters[k] += 1
 	a = []
+	mincount = 100
 	for x in characters:
 		if x[-1] == "s" and x[:-1] in characters:
 			characters[x[:-1]] += characters[x]
 			a.append(x)
-		elif characters[x] <= 10:
+		elif characters[x] <= mincount:
 			a.append(x)
 	for x in a:
 		characters.pop(x)
 	return characters
 
-
-stopwords = get_stopwords()			 
+			 
 bookname = input()
 with open(bookname, "r") as f:
 	sep = ['.', '!', '?', '”', '“']
@@ -66,29 +66,6 @@ with open(bookname, "r") as f:
 				sentences.append(sen)
 			sen = ""
 	characters = get_characters(sentences)
-	print(characters)
-	m = set("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-	characters = {}
-	for x in sentences:
-		s = wordsfroms(x)
-		for i in range(len(s)):
-			k = checkapo(s[i])
-			if k and k[0] in m and i != 0 and k.lower() not in stopwords:
-				if k not in characters:
-					characters[k] = 1
-				else:
-					characters[k] += 1
-	a = []
-	for x in characters:
-		if x[-1] == "s" and x[:-1] in characters:
-			characters[x[:-1]] += characters[x]
-			a.append(x)
-		elif characters[x] <= 10:
-			a.append(x)
-	for x in a:
-		characters.pop(x)
-	print()
-	print(characters)
 	meetings = {}
 	for x in sentences:
 		m = set()
@@ -96,21 +73,23 @@ with open(bookname, "r") as f:
 		for word in s:
 			if word in characters:
 				m.add(word)
-		for ch in m:
+		for n1 in m:
 			m1 = m.copy()
-			m1.remove(ch)
-			if ch not in meetings.keys():
-				meetings[ch] = m1
-			else:
-				meetings[ch] |= m1
+			m1.remove(n1)
+			if n1 not in meetings:
+				meetings[n1] = {}
+			for nx in m1:
+				if nx not in meetings[n1]:
+					meetings[n1][nx] = 1
+				else:
+					meetings[n1][nx] += 1
 	k = 0
 	for x in meetings:
 		k += 1
 		print(x, ":")
 		for y in meetings[x]:
-			print("    ", y)
-		if k > 4:
-			break
+			print("    ", y, "-", meetings[x][y])
+		
 
 
 
